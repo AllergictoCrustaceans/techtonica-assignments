f6c96d776e757e029c1c2a2c657adbf22ed55b5e
Katie Wu <katie.wy.wu@gmail.com>
2019-08-08 15:36:45 -0700

    Copy and paste mini OOP project from techtonica curriculum.

--- OOPinJS/index.js
+++ OOPinJS/index.js
@@ -3,7 +3,6 @@ class Ticket {
         this._ticketType = ticketType
         this._ticketPrice = ticketPrice;
     }
-
 }
 
 class Event {
@@ -21,38 +20,31 @@ class Event {
 
     searchTickets(lowNum, upNum) {
         let results = '';
-        if (ticketPrice > lowNum && ticketPrice < upNum) {
-            return 'Eligible tickets : <li>${Event.ticketType}</li>';
-        } else {
-            return 'No Tickets Available.';
+        for(let i = 0; i < this._availableTickets.length; i++) {
+            console.log(this._availableTickets[i]._ticketPrice);
+            if(this._availableTickets[i]._ticketPrice >= lowNum && this._availableTickets[i]._ticketPrice <= upNum) {
+                // return results += addAvailableTickets[i].ticketPrice + ", ";
+                console.log(this._availableTickets[i]._ticketPrice);
+            } else {
+                return 'No tickets available';
+            }
         }
     }
 
     cheapestTickets(cheapest) {
-        // get all the ticketPrice into an empty array. 
-        const cheapestTicketArray = new Array();
-        cheapestTicketArray.push(Event.ticketPrice);
-        var result = Math.min(...array);
-        return result;
+        // var result = Math.min(...array);
+        // return result;
     }
 }
 
-const event_obj1 = new Event("KLOS Golden Gala", "An evening with hollywood vampires");
-const event_obj2 = new Event("Skillet & Sevendust", "Victorious war tour");
-const event_obj3 = new Event("Jenny Lewis", "On the line tour 2019");
-
-const event_array = new Array();
-
-event_array.push(event_obj1, event_obj2, event_obj3);
-console.log(event_array);
+const event_obj1 = new Event("KLOS Golden Gala", "08.08.2019", "An evening with hollywood vampires");
+const event_obj2 = new Event("Skillet & Sevendust", "08.08.2019" , "Victorious war tour");
+const event_obj3 = new Event("Jenny Lewis", "08.08.2019", "On the line tour 2019");
 
 event_obj1.addAvailableTickets("human", 299);
 event_obj1.addAvailableTickets("vampire", 99);
 
 console.log(event_obj1);
-
-
-
 
 
 // $(document).ready(function() {
@@ -71,11 +63,21 @@ event_obj2.addAvailableTickets("Mezzanine", 200)
 event_obj2.addAvailableTickets("Balcony", 100)
 console.log(event_obj2);
 
+
+
+const event_array = new Array();
+
+event_array.push(event_obj1, event_obj2, event_obj3);
+
+
 $(document).ready(function () {
     let html = "";
-    $.each(event_array, function (index, item) {
-        html += `<li>${item.name} - ${item.description}</li>`;
+    $.each(event_array, function (index, item, ticketType, ticketPrice) {
+        html += `<li>${item._name} - ${item._description}</li> - Eligible Tickets: <li>${item._ticketType}</li>`;
     });
     // insert final html into #event...
     $("#event").html(html);
 });
+
+const what = event_obj1.searchTickets(25, 200);
+console.log(what);
