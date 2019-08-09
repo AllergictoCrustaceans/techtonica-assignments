e52d9a86ccd211c984d20282e313534643ade17d
Katie Wu <katie.wy.wu@gmail.com>
2019-08-09 16:32:25 -0700

    Modify src link back to static/js/myScript.js again.

--- techtonica-projects/jquery-play/static/js/myScript.js
+++ techtonica-projects/jquery-play/static/js/myScript.js
@@ -1,21 +1,30 @@
 
 $(document).ready(function() {
-    // console.log('myScript.js loaded!');
-    $('#red-sentence').click(function() {
-      // First remove the purple-text class in case it was added first.
-      $(this).removeClass('purple-text');
-      // Then add the red-text class to turn the sentence red.
-      $(this).addClass('red-text');
-    });
-  
-    $('#blue-button').click(function() {
-      // We can also "chain" methods together like this:
-      $('#blue-sentence')
-      .removeClass('purple-text')
-      .addClass('blue-text');
-    });
-  
-    $('#purple-button').click(function() {
-      $('.colorful-sentence').addClass('purple-text');
-    });
+  // console.log('myScript.js loaded!');
+  $('#red-sentence').click(function() {
+    // First remove the purple-text class in case it was added first.
+    $(this).removeClass('purple-text');
+    // Then add the red-text class to turn the sentence red.
+    $(this).addClass('red-text');
   });
+
+  $('#blue-button').click(function() {
+    // We can also "chain" methods together like this:
+    $('#blue-sentence')
+    .removeClass('purple-text')
+    .addClass('blue-text');
+  });
+
+  $('#purple-button').click(function() {
+    $('.colorful-sentence').addClass('purple-text');
+  });
+});
+
+$(document).ready(function(){
+  $("#blue-sentence").hover(function(){
+    $(this).css("font-size", "28px");
+    $(this).css("background-color", "yellow");
+    }, function(){
+    $(this).css("background-color", "white");
+  });
+});
