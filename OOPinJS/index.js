aea09d32c76bce4ad87fc880a5ed6796cf01efe7
Katie Wu <katie.wy.wu@gmail.com>
2019-08-10 21:47:53 -0700

    Edit searchTickets() and cheapestTickets().

--- OOPinJS/index.js
+++ OOPinJS/index.js
@@ -21,19 +21,25 @@ class Event {
     searchTickets(lowNum, upNum) {
         let results = '';
         for(let i = 0; i < this._availableTickets.length; i++) {
-            console.log(this._availableTickets[i]._ticketPrice);
-            if(this._availableTickets[i]._ticketPrice >= lowNum && this._availableTickets[i]._ticketPrice <= upNum) {
-                // return results += addAvailableTickets[i].ticketPrice + ", ";
-                console.log(this._availableTickets[i]._ticketPrice);
-            } else {
-                return 'No tickets available';
-            }
+            var range = this._availableTickets[i]._ticketPrice > lowNum && this._availableTickets[i]._ticketPrice < upNum;
+            console.log(range? results += this._availableTickets[i]._ticketType + ', ': 'No tickets available');
         }
+        return results;
     }
 
-    cheapestTickets(cheapest) {
-        // var result = Math.min(...array);
-        // return result;
+    cheapestTickets() {
+        let ticketArray = []; 
+        for(let i = 0; i < event_array.length; i++) {
+            for(let j = 0; j < event_array[i]._availableTickets.length; j++) {
+                var item = event_array[i]._availableTickets[j]._ticketPrice;
+                ticketArray.push(item);
+                var cheapest = Math.min(...ticketArray);
+                //find the ticket type that includes the cheapest price variable.
+                event_array.find(function(cheapest) {
+                    console.log(item.includes(cheapest));
+                });
+            }
+        }
     }
 }
 
@@ -43,9 +49,29 @@ const event_obj3 = new Event("Jenny Lewis", "08.08.2019", "On the line tour 2019
 
 event_obj1.addAvailableTickets("human", 299);
 event_obj1.addAvailableTickets("vampire", 99);
-
 console.log(event_obj1);
 
+
+event_obj2.addAvailableTickets("General Admission", 25);
+event_obj2.addAvailableTickets("Floor Seating", 80);
+event_obj2.addAvailableTickets("Orchestra", 300);
+event_obj2.addAvailableTickets("Mezzanine", 200);
+event_obj2.addAvailableTickets("Balcony", 100);
+console.log(event_obj2);
+
+
+const event_array = new Array();
+event_array.push(event_obj1, event_obj2, event_obj3);
+
+
+$(document).ready(function () {
+    let html = "";
+    $.each(event_array, function (index, item) {
+        html += `<li>${item._name} - ${item._description} - Eligible Tickets: ${item._availableTickets}` ;
+    });
+    // insert final html into #event...
+    $("#event").html(html);
+});
 
 // $(document).ready(function() {
 //     let text = "";
@@ -55,29 +81,4 @@ console.log(event_obj1);
 //     $("#event").html(text);
 // });
 
-
-event_obj2.addAvailableTickets("General Admission", 25)
-event_obj2.addAvailableTickets("Floor Seating", 80)
-event_obj2.addAvailableTickets("Orchestra", 300)
-event_obj2.addAvailableTickets("Mezzanine", 200)
-event_obj2.addAvailableTickets("Balcony", 100)
-console.log(event_obj2);
-
-
-
-const event_array = new Array();
-
-event_array.push(event_obj1, event_obj2, event_obj3);
-
-
-$(document).ready(function () {
-    let html = "";
-    $.each(event_array, function (index, item, ticketType, ticketPrice) {
-        html += `<li>${item._name} - ${item._description} - Eligible Tickets: ${item._ticketType}</li>`;
-    });
-    // insert final html into #event...
-    $("#event").html(html);
-});
-
-const what = event_obj1.searchTickets(25, 200);
-console.log(what);
+event_obj2.searchTickets(25, 200);
