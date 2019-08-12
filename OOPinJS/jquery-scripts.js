724e297c3ab74fe23b61d998dcd8cb64c7f6da17
Katie Wu <katie.wy.wu@gmail.com>
2019-08-11 22:15:00 -0700

    Create jQuery functions to add rows of events and usernames.

--- OOPinJS/jquery-scripts.js
+++ OOPinJS/jquery-scripts.js
@@ -1,22 +1,28 @@
 //FOR BACKGROUND VIDEO PAUSE
 var free = document.getElementById('free');
-var videoButton = document.getElementById('videoButton');
-
-function pause() {
-    if(video.paused) {
-        video.play();
-        videoButton.innerHTML = "Pause";
-    } else {
-        video.pause();
-        videoButton.innerHTML = "Play";
-    }
-}
-
 
 
 //default list of tour events within the modal (these cannot be deleted)
     //this has a + and - sign next to the tour events. 
-
+    $(document).ready(function(){
+        $(".add-row").click(function(){
+            var eventName = $("#eventName").val();
+            var eventDate = $("#eventDate").val();
+            var eventDescription = $("#eventDescription").val();
+            var eventPrice = $("#eventPrice").val();
+            var markup = "<tr><td><input type='checkbox' name='record'></td><td>" + eventDate + "</td><td>" + eventName + "</td><td>" + eventDescription + "</td><td>" + eventPrice + "</td></tr>";
+            $("table tbody").append(markup);
+        });
+        
+        // Find and remove selected table rows
+        $(".delete-row").click(function(){
+            $("table tbody").find('input[name="record"]').each(function(){
+            	if($(this).is(":checked")){
+                    $(this).parents("tr").remove();
+                }
+            });
+        });
+    });
 
 // button to add text input of user (these can be deleted)
     //User click generate to generate a user row onto the bottom of the modal,
