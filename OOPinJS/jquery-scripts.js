a71afb8f5bd7af73bc3faf35de08c4cbc7231ced
Katie Wu <katie.wy.wu@gmail.com>
2019-08-11 18:07:22 -0700

    Create click function for class add.

--- OOPinJS/jquery-scripts.js
+++ OOPinJS/jquery-scripts.js
@@ -8,16 +8,27 @@ function pause() {
         videoButton.innerHTML = "Pause";
     } else {
         video.pause();
-        pause.innerHTML = "Play";
+        videoButton.innerHTML = "Play";
     }
 }
+
+
 
 //default list of tour events within the modal (these cannot be deleted)
     //this has a + and - sign next to the tour events. 
 
 
 // button to add text input of user (these can be deleted)
-    //User click generate to generate a user row onto the bottom of the modal
+    //User click generate to generate a user row onto the bottom of the modal,
+    $('.add').on('click', function() {
+        var inputName = $('#basic').val();
+        const user1 = new User(inputName);
+        eventRecommender.addUser(user1);
+        console.log(eventRecommender);
+        //clear userInput after adding
+        //make sure user shows up on the bottom portion of the modal.
+        // delete user button on the right if row is selected. 
+    });
     // - will delete user from row on the bottom of the modal
 
 
