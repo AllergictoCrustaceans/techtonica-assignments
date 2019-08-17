1a45be96ff10421d3eb62bdc88ca215579f36146
Katie Wu <katie.wy.wu@gmail.com>
2019-08-16 19:50:21 -0700

    add array of users; create .post function.

--- techtonica-labs/getting-started-with-node/app.js
+++ techtonica-labs/getting-started-with-node/app.js
@@ -26,11 +26,43 @@ app.use(bodyParser.urlencoded({extended: false}));
 //SET STATIC PATH 
 app.use(express.static(path.join(__dirname, 'public'))); // might need to make a directory called public
 
+var users = [
+    {
+        id: 1,
+        first_name: 'John',
+        last_name: 'Doe',
+        email: 'johndoe@gmail.com',
+    },
+    {
+        id: 2,
+        first_name: 'Bob',
+        last_name: 'Smith',
+        email: 'bobsmith@gmail.com',
+    },
+    {
+        id: 3,
+        first_name: 'Jill',
+        last_name: 'Jackson',
+        email: 'jjackson@gmail.com',
+    }
 
+]
 
 
 app.get('/', function(req, res) {
-    res.render('index');
+    res.render('index', {
+        title: 'Customers',
+        users: users
+    });
+});
+
+app.post('/users/add', function(req, res) {
+    var newUser = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email
+    }
+    console.log(newUser);
 });
 
 app.listen(3000, function() {
