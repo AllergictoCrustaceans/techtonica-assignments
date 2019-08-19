30252616813fbc786ce7eff10c011be62483a5f2
Katie Wu <katie.wy.wu@gmail.com>
2019-08-19 15:58:33 -0700

    Create members.js file.

---
+++ techtonica-labs/express-tutorial/members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'Andorra Pacheco',
+        email: 'apacheco@haptmon.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Andromeda Luna',
+        email: 'aluna@haptmon.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Andronico Sol',
+        email: 'asol@haptmon.com',
+        status: 'active'
+    },
+]
+
+module.exports = members;
