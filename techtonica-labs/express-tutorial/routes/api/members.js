9412fd0ab9e0a54fc6a83a11e1efbf9142bfc17e
Katie Wu <katie.wy.wu@gmail.com>
2019-08-19 22:49:58 -0700

    Add update and delete member function.

--- techtonica-labs/express-tutorial/routes/api/members.js
+++ techtonica-labs/express-tutorial/routes/api/members.js
@@ -32,8 +32,41 @@ router.post('/', (req, res) => {
         return res.status(400).json({ msg: 'Please include a name and email'});
     } 
 
+    
     members.push(newMember);
     res.json(members);
+});
+
+//UPDATE MEMBER
+router.put('/:id', (req,res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
+
+    if(found) {
+        const updateMember = req.body;
+        members.forEach(member => {
+            if(member.id === parseInt(req.params.id)) {
+                member.name = updateMember.name? updateMember.name : member.name;
+                member.email = updateMember.email? updateMember.email : member.email;
+
+                res.json({ msg: 'Member updated', member});
+            }
+        })
+    } else {
+        res.status(400).json({ msg: `No member with the id of ${req.params.id} found`});
+    }
+});
+
+//DELETE MEMBER
+router.delete('/:id', (req,res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
+
+    if(found) {
+        res.json({ 
+            msg: 'Member deleted', members: members.filter(member => member.id !== parseInt(req.params.id))
+        });
+    } else {
+        res.status(400).json({ msg: `No member with the id of ${req.params.id} found`});
+    }
 });
 
 module.exports = router;
