a6bbc7b960289187de761964ba4da8dee37c47b7
Katie Wu <katie.wy.wu@gmail.com>
2019-08-19 23:09:56 -0700

    Require ./Members and create homepage route function.

--- techtonica-labs/express-tutorial/index.js
+++ techtonica-labs/express-tutorial/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const logger = require('./middleware/logger');
+const members = require('./Members');
 
 const app = express();
 
@@ -16,6 +17,13 @@ app.set('view engine', 'handlebars');
 //BODY PARSER
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+//HOMEPAGE ROUTE
+app.get('/', (req,res) => res.render('index', {
+    title: 'Member App',
+    members
+}));
+
 
 //SET STATIC FOLDER
 app.use(express.static(path.join(__dirname, 'public')));
