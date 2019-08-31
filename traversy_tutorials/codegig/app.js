04d51bd7aae341d1f1560667767d69d49170d7e5
Katie Wu <katie.wy.wu@gmail.com>
2019-08-31 14:49:22 -0700

    Add handlebars functions.

--- traversy_tutorials/codegig/app.js
+++ traversy_tutorials/codegig/app.js
@@ -8,12 +8,17 @@ const db = require('./config/database');
 
 const app = express();
 
+
+//Handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 //Test DB
 db.authenticate()
     .then(() => console.log('Database connected...'))
     .catch(err => console.log('Error:' + err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5432;
 
 app.get('/', (req, res) => {
     res.send('INDEX');
