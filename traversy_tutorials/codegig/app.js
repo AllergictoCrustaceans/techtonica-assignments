bf4b6103ab45458e1679aef4502a355ad7048725
Katie Wu <katie.wy.wu@gmail.com>
2019-08-31 12:34:59 -0700

    Create database authenticate(), and require ./config/database.

--- traversy_tutorials/codegig/app.js
+++ traversy_tutorials/codegig/app.js
@@ -3,9 +3,17 @@ const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+//Database 
+const db = require('./config/database');
+
 const app = express();
 
-const PORT = process.env.PORT || 5432;
+//Test DB
+db.authenticate()
+    .then(() => console.log('Database connected...'))
+    .catch(err => console.log('Error:' + err));
+
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.send('INDEX');
