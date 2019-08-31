0b885586f0cc5854c695522bf4e11353f1007e09
Katie Wu <katie.wy.wu@gmail.com>
2019-08-31 14:49:54 -0700

    Hardcode data to be inserted into the codegig db via router.get().

--- traversy_tutorials/codegig/routes/gigs.js
+++ traversy_tutorials/codegig/routes/gigs.js
@@ -3,12 +3,38 @@ const router = express.Router();
 const db = require('../config/database');
 const Gig = require('../models/Gig');
 
+//Get gig list
 router.get('/', (req, res) => {
     Gig.findAll()
         .then(gigs => {
         console.log(gigs);
         res.sendStatus(200);
         })
+        .catch(err => console.log(err));
+})
+
+
+//Add a gig
+router.get('/add', (req, res) => {
+    const data = {
+        title: 'Looking for a React developer',
+        technologies: 'react,javascript,html,css',
+        budget: '$3000',
+        description: 'Unlike recruiters, we look for at portfolios, not years of experience',
+        contact_email: 'user1@gmail.com'
+    }
+
+    let {title, technologies, budget, description, contact_email} = data;
+
+    //Insert into table
+    Gig.create({
+        title,
+        technologies,
+        description,
+        budget,
+        contact_email
+    })
+        .then(gig => res.redirect('/gigs'))
         .catch(err => console.log(err));
 })
 
