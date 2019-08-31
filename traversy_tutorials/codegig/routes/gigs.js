de0698c6fa0d87ed2ee2973b498c9753cc6b6301
Katie Wu <katie.wy.wu@gmail.com>
2019-08-31 15:21:18 -0700

    get gig list with gigs.handlebars, and display add gig form with add.handlebars.

--- traversy_tutorials/codegig/routes/gigs.js
+++ traversy_tutorials/codegig/routes/gigs.js
@@ -7,15 +7,21 @@ const Gig = require('../models/Gig');
 router.get('/', (req, res) => {
     Gig.findAll()
         .then(gigs => {
-        console.log(gigs);
-        res.sendStatus(200);
+            res.render('gigs', {
+                gigs
+            });
         })
         .catch(err => console.log(err));
 })
 
+//Disply add gig form
+router.get('/add', (req, res) => {
+    res.render('add');
+})
+
 
 //Add a gig
-router.get('/add', (req, res) => {
+router.post('/add', (req, res) => {
     const data = {
         title: 'Looking for a React developer',
         technologies: 'react,javascript,html,css',
