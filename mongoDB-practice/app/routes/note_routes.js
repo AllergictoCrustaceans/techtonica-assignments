82116f876bdb0721e5563e6300f5373da77be471
Katie Wu <katie.wy.wu@gmail.com>
2019-09-03 16:42:30 -0700

    Take ; out.

--- mongoDB-practice/app/routes/note_routes.js
+++ mongoDB-practice/app/routes/note_routes.js
@@ -5,7 +5,7 @@ module.exports = function(app, db) {
     app.get('/notes/:id', (req, res) => {
         const id = req.params.id;
         const details = {
-            '_id': new ObjectID(id);
+            '_id': new ObjectID(id)
         };
         db.collection('notes').findOne(details, (err, item) => {
             if(err) {
