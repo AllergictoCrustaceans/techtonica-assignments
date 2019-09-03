aa39df774f1a4042d91afda3fcd72513b618815c
Katie Wu <katie.wy.wu@gmail.com>
2019-09-03 12:26:39 -0700

    Add body-parser and created route.

--- mongoDB-practice/server.js
+++ mongoDB-practice/server.js
@@ -6,5 +6,12 @@ const app = express();
 
 const PORT = 8000;
 
+//bodyParser
+app.use(bodyParser.urlencoded({extended:true}));
+
+//import routes folder 
+require('./app/routes')(app, {});
+
+
 //listen 
 app.listen(PORT, () => {console.log(`Server started on ${PORT}`);});
